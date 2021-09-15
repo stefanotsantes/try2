30e9502e5a7a4a4239357c3a7be8fffb76060525
Andreaziterni <paolipaolo45@gmail.com>
2021-09-15 20:31:46 +0200

    fdfdfdadad

--- app/src/main/java/com/example/try2/login.java
+++ app/src/main/java/com/example/try2/login.java
@@ -1,5 +1,6 @@
 package com.example.try2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -7,6 +8,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,11 +20,37 @@ import com.example.try2.databinding.ActivityLoginBinding;
 
 public class login extends AppCompatActivity {
 
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+
     private AppBarConfiguration appBarConfiguration;
     private ActivityLoginBinding binding;
 
+
+    private void validate (java.lang.String userName, java.lang.String userPassword){
+        if ((userName == "Admin") && (userPassword == "sonogay")) {
+            Intent intent = new Intent(login.this, SecondActivity.class);
+            startActivity(intent);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Name = (EditText) findViewById(R.id.Name);
+        Password = (EditText) findViewById(R.id.Password);
+        Login = (Button) findViewById(R.id.Login);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+
+
         super.onCreate(savedInstanceState);
 
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
@@ -32,20 +61,5 @@ public class login extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_login);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }}
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_login);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-}
